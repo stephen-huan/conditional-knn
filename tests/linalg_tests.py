4632cc500a023daa149c1820534e9fa2adb5148b
Stephen Huan <stephen.huan@cgdct.moe>
2024-11-30 03:15:28 -0800

    test(tests/linalg_tests): test on general matrices

--- tests/linalg_tests.py
+++ tests/linalg_tests.py
@@ -1,13 +1,12 @@
 import time
 
 import numpy as np
-import sklearn.gaussian_process.kernels as kernels
 
 from KoLesky import linalg
 
 D = 3  # dimension of points
-N = 50  # number of points
-M = 4_000  # number of points
+M = 1000
+N = 2000
 
 # display settings
 np.set_printoptions(precision=3, suppress=True)
@@ -16,24 +15,16 @@ np.set_printoptions(precision=3, suppress=True)
 rng = np.random.default_rng(1)
 
 if __name__ == "__main__":
-    X = rng.random((N, D))
-    # generate a symmetric positive definite matrix
-    kernel = kernels.Matern(length_scale=1, nu=5 / 2)
-    theta: np.ndarray = kernel(X)  # type: ignore
-
+    A = rng.random((M, N))
     assert np.isclose(
-        linalg.operator_norm(rng, theta), np.linalg.norm(theta, ord=2)
+        linalg.operator_norm(rng, A), np.linalg.norm(A, ord=2)
     ), "power method is not correct"
 
-    X = rng.random((M, D))
-    theta: np.ndarray = kernel(X)  # type: ignore
-
+    A = rng.random((N * 2, M * 2))
     start = time.time()
-    ans = np.linalg.norm(theta, ord=2)
+    ans = np.linalg.norm(A, ord=2)
     print(f"{time.time() - start:.3f}")
     start = time.time()
-    print(time.time() - start)
-    start = time.time()
-    ans2 = linalg.operator_norm(rng, theta)
+    ans2 = linalg.operator_norm(rng, A)
     print(f"{time.time() - start:.3f}")
     assert np.isclose(ans, ans2), "power method is not correct"
