caba95659b8497b8c6059c85baae24efe0b9e378
Stephen Huan <stephen.huan@cgdct.moe>
2024-11-30 03:14:54 -0800

    feat(KoLesky/linalg): general operator norm

--- KoLesky/linalg.py
+++ KoLesky/linalg.py
@@ -10,7 +10,7 @@ def frobenius_norm(A: Matrix) -> float:
     return norm(A, ord="fro")  # type: ignore
 
 
-def operator_norm(
+def __operator_norm(
     rng: np.random.Generator, A: Matrix | LinearOperator, eps: float = 1e-8
 ) -> float:
     """Operator norm of A by power method."""
@@ -22,3 +22,22 @@ def operator_norm(
         x = y / norm(y)
         y: np.ndarray = A @ x  # type: ignore
     return np.abs(np.inner(y, x))
+
+
+def operator_norm(
+    rng: np.random.Generator,
+    A: Matrix | LinearOperator,
+    eps: float = 1e-8,
+    hermitian: bool = False,
+) -> float:
+    """Operator norm of A by power method."""
+    if hermitian:
+        return __operator_norm(rng, A, eps)
+    else:
+        n, m = A.shape
+        B = (
+            LinearOperator((n, n), matvec=lambda x: A @ (A.T @ x))
+            if n < m
+            else LinearOperator((m, m), matvec=lambda x: A.T @ (A @ x))
+        )
+        return np.sqrt(__operator_norm(rng, B, eps))
